4bd12fbd59e772e2b7ba485a183a81a7b5b279ea
yahalou <77041674+yahalou@users.noreply.github.com>
2023-12-10 15:45:24 +0800

    项目初始结构创建

---
+++ gulimall-member/src/test/java/com/cl/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cl.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
